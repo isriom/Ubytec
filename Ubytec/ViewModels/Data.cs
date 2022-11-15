f3fa5a4e609a21dee4ac87e3fcb8dc0aac49e796
isriom <Isriom@estudiantec.cr>
2022-11-14 22:52:07 -0600

    Data class to let usr acces

---
+++ Ubytec/ViewModels/Data.cs
@@ -0,0 +1,47 @@
+﻿namespace Ubytec.ViewModels;
+
+/**
+ * Clase donde se maneja toda la estructura de los datos que se van a almacenar en la base de datos
+ */
+public class Data
+{
+
+    /**
+     * Clase que manipula informacion del login
+     */
+    public class LoginUser
+    {
+        /**
+         * Constructor
+         * <param name="role"></param>
+         */
+        public LoginUser()
+        {
+            username = "";
+            password = "";
+            role = "";
+        }
+
+        public string role { get; set; }
+
+        /**
+         * Metodo  para obtener la info
+         */
+        public LoginUser(string password = null, string username = null, string role = null)
+        {
+            this.password = password;
+            this.username = username;
+            this.role = role;
+        }
+
+        /**
+         * Metodo get y set para manejo del username
+         */
+        public string username { get; set; }
+
+        /**
+         * Metodo get y set para manejo del Constrasena
+         */
+        public string password { get; set; }
+    }
+}
---
+++ Ubytec/ViewModels/Data_Class.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using Ubytec.Models;
+
+namespace Ubytec.ViewModels;
+
+public abstract class Element
+{
+    internal static ubytecContext Context { get; set; } = null;
+}
+
+public class AdminData
+{
+}
